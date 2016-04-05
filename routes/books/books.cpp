901413c6cc62bdba02ddb2c547d8ea5b817f412e
blkshdw <aleksey.glebow@gmail.com>
2016-04-05 11:50:11 +0300

    prgrs

--- routes/books/books.cpp
+++ routes/books/books.cpp
@@ -25,9 +25,9 @@ crow::response BooShelf::Route::getBooks(std::shared_ptr<RethinkDB::Connection> 
     }
     try {
         R::Cursor cur = db.table("books").order_by(R::desc("popularity")).filter(
-                (R::row["author"].match(author_filter), R::) &&
-                (R::expr(R::row["title"].not_()) || R::row["title"].match(title_filter)) &&
-                (R::expr(R::row["title"].not_()) || R::row["genre"].match(genre_filter))).limit(1000).run(*conn);
+                (R::row["author"].match(author_filter)) &&
+                (R::row["title"].match(title_filter)) &&
+                (R::row["genre"].match(genre_filter))).opt(R::optargs("default", true)).limit(1000).run(*conn);
         return crow::response(R::write_datum(cur.to_datum()));
     } catch(R::Error err) {
         throw Http::DataBaseException(err.message);
